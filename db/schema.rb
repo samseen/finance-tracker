4e45d3744e8b408ccf478f64f7b2ae0cef7443d3
Samson Akanbi <samson.akanbi@outlook.com>
2017-02-16 14:28:03 +0000

    Update views and navigation

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170216073717) do
+ActiveRecord::Schema.define(version: 20170216131625) do
 
   create_table "stocks", force: :cascade do |t|
     t.string   "ticker"
@@ -41,6 +41,8 @@ ActiveRecord::Schema.define(version: 20170216073717) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "first_name"
+    t.string   "last_name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
